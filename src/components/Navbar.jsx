103c77029d809b7603883026d5a3b51e7eaf7645
tanya050803 <158026894+tanya050803@users.noreply.github.com>
2025-03-26 07:39:52 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     </div>
     
     <div className="m-8 flex items-center justify-center gap-5 text-2xl">
-      <a href="https://drive.google.com/file/d/1VxUblUqBof0wkSjJHwQoPZu7IB2SsP4o/view?usp=sharing" download="TanyaJain_resume.pdf" className="relative flex items-center"><FaDownload></FaDownload></a>
+      <a href="https://drive.google.com/file/d/13YzvO_zwHNa61TudkadZvQ99tJj6lCFy/view?usp=sharing" download="TanyaJain_resume.pdf" className="relative flex items-center"><FaDownload></FaDownload></a>
       <span className="absolute"></span>
       <a href="https://www.linkedin.com/in/tanya-jain-88257a238/" target="_blank"><FaLinkedin></FaLinkedin></a>
       <a href="https://github.com/tanya050803" target="_black"><FaGithub></FaGithub></a>
